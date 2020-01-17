d2f326c1fd96823085009fa05efddc5006ed67fd
saeed abdollahian <saeedvir@users.noreply.github.com>
2020-01-17 18:14:48 +0330

    update routejs.php , add routes collections

--- config/routejs.php
+++ config/routejs.php
@@ -2,14 +2,20 @@
 
 return [
 	'app_variable'=>'RouteJs',
-	'js_file'=>public_path('routes.js'), //public/routes.js
+	'js_file'=>public_path('public/routes.js'), //public/routes.js
 	'export_all_routes'=>false, // if true then export all routes
 	'routes'=>[
+		'user'=>['home'],
+		'admin'=>['users.index'],
 		/*'home',
 		'blog',
 		'post',
 		'post.comment',
 		'list',*/
+	],
+	'js_files'=>[
+		'user'=>'routejs.user.js',
+		'admin'=>'routejs.admin.js',
 	],
 	'append_js'=>'', // for ex : 'var tmp="tmp_value";console.log(tmp);'
 ];
